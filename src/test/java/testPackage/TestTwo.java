f4e413fc6964b0a70ceabaac9a81e1c502bd4ad9
manaswitha <manaswitha.danda@pratian.com>
2020-01-06 11:12:29 +0530

    TWO Test Classes

---
+++ src/test/java/testPackage/TestTwo.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class TestTwo {
+	
+	@Test
+	public void test5() {
+		System.out.println("C2: TC5");
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test6() {
+		System.out.println("C2: TC6");
+		assertTrue(true);
+	}
+}
